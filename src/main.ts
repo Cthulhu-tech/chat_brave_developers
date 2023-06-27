82a0b117aa94d40f65d8e676e6ef0e753d67c65a
Ratctangle <90329246+Cthulhu-tech@users.noreply.github.com>
2023-06-28 01:29:26 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     optionsSuccessStatus: 200,
   })
   app.use(cookieParser())
-  await app.listen(3000)
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
