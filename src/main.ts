585b5421907afd611bc2dc41888d444b7bf83655
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-07-06 02:07:55 +0400

    update class validator

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalPipes(new ValidationPipe({
+		transform: true,
+	}));
 	app.enableCors({
 		origin: process.env.origin,
 		methods: "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS",
